ece821683a8eb7fbf94f2c68b3d7db798094f183
chromalemon <nehon9914@gmai.com>
2026-07-04 14:45:55 +0100

    Vim auto-save: Sat Jul  4 14:45:55 BST 2026

--- include/bmp.h
+++ include/bmp.h
@@ -5,24 +5,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BMP_DEFAULTS = { .file_type = 0x4D42, .reserved1 = 0, .reserved2 = 0, .offset = 54, .DIB_size = 40, .planes = 1, .compression = 0, .x_res = 2835, .y_res = 2835, .colors_used = 0, .colors_important = 0 }
+
 #pragma pack(push, 1)
 typedef struct BMP_meta {
-	uint16_t file_type = 0x4D42;
+	uint16_t file_type;
 	uint32_t file_size;
-	uint16_t reserved1 = 0;
-	uint16_t reserved2 = 0;
-	uint32_t offset = 54;
-	uint32_t DIB_size = 40;
+	uint16_t reserved1;
+	uint16_t reserved2;
+	uint32_t offset;
+	uint32_t DIB_size;
 	uint32_t width;
 	uint32_t height;
-	uint16_t planes = 1;
+	uint16_t planes;
 	uint16_t BPP;
-	uint32_t compression = 0;
+	uint32_t compression;
 	uint32_t image_size;
-	uint32_t x_res = 2835;
-	uint32_t y_res = 2835;
-	uint32_t colors_used = 0;
-	uint32_t colors_important = 0;
+	uint32_t x_res;
+	uint32_t y_res;
+	uint32_t colors_used;
+	uint32_t colors_important;
 } BMP_meta;
 #pragma pack(pop)
 
