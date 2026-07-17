331e6b25a7edb0a6d95de4988a41bec32033372d
chromalemon <nehon9914@gmai.com>
2026-07-17 16:26:35 +0100

    Vim auto-save: Fri Jul 17 16:26:35 BST 2026

--- tests/test_main.c
+++ tests/test_main.c
@@ -5,15 +5,13 @@ void test_single_character_decompression(void);
 void test_no_repeat_compression(void);
 void test_no_repeat_decompression(void);
 
+int main(void) {
+  UNITY_BEGIN();
 
-int main(void)
-{
-    UNITY_BEGIN();
+  RUN_TEST(test_single_character_compression);
+  RUN_TEST(test_single_character_decompression);
+  RUN_TEST(test_no_repeat_compression);
+  RUN_TEST(test_no_repeat_decompression);
 
-    RUN_TEST(test_single_character_compression);
-    RUN_TEST(test_single_character_decompression);
-    RUN_TEST(test_no_repeat_compression);
-    RUN_TEST(test_no_repeat_decompression);
-
-    return UNITY_END();
+  return UNITY_END();
 }
